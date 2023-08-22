91d8373264881ebc0a4071fc9d48355c585534a4
soohyang136 <jsh515228@gmail.com>
2023-08-22 12:03:46 +0900

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,6 +17,7 @@ const BlogIndex = ({ data, location }) => {
           No blog posts found. Add markdown posts to "content/blog" (or the
           directory you specified for the "gatsby-source-filesystem" plugin in
           gatsby-config.js).
+          asd
         </p>
       </Layout>
     )
