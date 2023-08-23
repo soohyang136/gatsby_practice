7bd776cfcd8b149febae3346ae44153fe3a96e26
soohyang136 <jsh515228@gmail.com>
2023-08-23 09:42:49 +0900

    FEAT :: main page introduction

---
+++ src/components/main/ProfileImage.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from "react";
+import styled from '@emotion/styled'
+
+const PROFILE_IMAGE_LINK = "https://avatars.githubusercontent.com/u/62911285?s=400&u=605d89efecd8ec0b426a3579aeb901a7ffea81c2&v=4"
+
+const ProfileImageWrapper = styled.img`
+    width: 120px;
+    height: 120px;
+    margin-bottom: 30px;
+    border-radius: 50%;
+`
+
+const ProfileImage: FunctionComponent = function() {
+    return <ProfileImageWrapper  src={PROFILE_IMAGE_LINK} alt="Profile Image"/>
+}
+
+export default ProfileImage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
 import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+import GlobalStyle from '../components/common/GlobalStyle'
+import Introduction from '../components/main/Introduction'
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+`
 
 const IndexPage: FunctionComponent = function () {
-  return <div>home</div>
+  return (
+    <Container>
+        <GlobalStyle />
+        <Introduction />
+    </Container>
+  )
 }
 
 export default IndexPage
