2a084e005eb5fe5ad1ec7cc088ba9daa7b0c8ddb
soohyang136 <jsh515228@gmail.com>
2023-08-23 11:25:23 +0900

    FEAT :: categories, posts in main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,15 @@ import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 import GlobalStyle from '../components/common/GlobalStyle'
 import Introduction from '../components/main/Introduction'
+import Footer from '../components/common/Footer'
+import CategoryList from '../components/main/CategoryList'
+import PostList from '../components/main/PostList'
+
+const CATEGORY_LIST = {
+    All: 5,
+    Web: 3,
+    Mobile: 2,
+  }
 
 const Container = styled.div`
     display: flex;
@@ -14,6 +23,9 @@ const IndexPage: FunctionComponent = function () {
     <Container>
         <GlobalStyle />
         <Introduction />
+        <CategoryList selectedCategory='Web' categoryList={CATEGORY_LIST} />
+        <PostList />
+        <Footer />
     </Container>
   )
 }
